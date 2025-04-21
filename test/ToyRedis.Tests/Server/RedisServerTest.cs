d951ac97338e7dd0389cf38ce4054cd74d61a6e4
Elias Castro <38994457+EliasSarges@users.noreply.github.com>
2025-04-20 21:33:41 -0300

    style: remove white spaces

--- test/ToyRedis.Tests/Server/RedisServerTest.cs
+++ test/ToyRedis.Tests/Server/RedisServerTest.cs
@@ -12,15 +12,15 @@ public class RedisServerTest
         var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(5));
         var server = new RedisServer();
         var serverTask = server.StartAsync();
-    
+
         // Wait for the server to be ready
         await server.Ready.Task;
 
         var tcpClient = new TcpClient("localhost", server.Port);
-        
+
         var stream = tcpClient.GetStream();
         var buffer = "*1\r\n$4\r\nPING\r\n"u8.ToArray();
-        
+
         await stream.WriteAsync(buffer, cancellationToken.Token);
 
         var responseBuffer = new byte[1024];
@@ -28,9 +28,9 @@ public class RedisServerTest
         var response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
 
         Assert.Equal("+PONG\r\n", response);
-        
+
         server.Stop();
-        
+
         // propagate the exception if any
         await serverTask;
     }
