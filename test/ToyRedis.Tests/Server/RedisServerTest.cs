e3f789fdd3990f2ff7d0faf43e291a9e2133ae29
Elias Castro <38994457+EliasSarges@users.noreply.github.com>
2025-04-20 21:47:29 -0300

    test: add persist client and persistent connection client tests

--- test/ToyRedis.Tests/Server/RedisServerTest.cs
+++ test/ToyRedis.Tests/Server/RedisServerTest.cs
@@ -34,4 +34,63 @@ public class RedisServerTest
         // propagate the exception if any
         await serverTask;
     }
+
+    [Fact]
+    public async Task ShouldPersistClientConnection()
+    {
+        var server = new RedisServer();
+        var serverTask = server.StartAsync();
+
+        await server.Ready.Task;
+
+        var tcpClient = new TcpClient("localhost", server.Port);
+
+        await using var stream = tcpClient.GetStream();
+        var buffer = "*1\r\n$4\r\nPING\r\n"u8.ToArray();
+
+        for (var i = 0; i < 2; i++)
+        {
+            using var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            await stream.WriteAsync(buffer, cancellationToken.Token);
+
+            var responseBuffer = new byte[1024];
+            var bytesRead = await stream.ReadAsync(responseBuffer, cancellationToken.Token);
+            var response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+
+            Assert.Equal("+PONG\r\n", response);
+        }
+
+        server.Stop();
+
+        await serverTask;
+    }
+
+    [Fact]
+    public async Task ShouldHandleMultipleClients()
+    {
+        var server = new RedisServer();
+        var serverTask = server.StartAsync();
+
+        await server.Ready.Task;
+
+        for (var i = 0; i < 2; i++)
+        {
+            var tcpClient = new TcpClient("localhost", server.Port);
+
+            await using var stream = tcpClient.GetStream();
+            var buffer = "*1\r\n$4\r\nPING\r\n"u8.ToArray();
+
+            using var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            await stream.WriteAsync(buffer, cancellationToken.Token);
+
+            var responseBuffer = new byte[1024];
+            var bytesRead = await stream.ReadAsync(responseBuffer, cancellationToken.Token);
+            var response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+
+            Assert.Equal("+PONG\r\n", response);
+        }
+
+        server.Stop();
+        await serverTask;
+    }
 }
